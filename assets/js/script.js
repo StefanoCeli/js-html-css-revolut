da13cce3d323c2ab811112f0b05cb7370f802935
StefanoCeli <stefanopsn91@gmail.com>
2021-04-21 21:57:40 +0200

    fine esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    //dichiaro una variabile per specificare il percorso nel quale andrò ad interagire
+    var link=$('.header-right>ul>li');
 
-
+    //qui uso il metodo mouseenter e gli dico tramite una funzione di trovare al suo interno l'elemento che contiene la classe dropdown e active,in modo che se la trova già attiva la va a togliere,dopodichè gli vado a dire che all'hover del mouse sul link di aggiungere la classe active.
+    //usando questo metodo faccio in modo che ogni volta che vado su un altro link quello precedente viene chiuso
+    link.mouseenter(function(){
+        link.find('.dropdown.active').removeClass('active');
+        $(this).find('.dropdown').addClass('active');
+    })
+   
+    //qui uso il metodo click e gli dico tramite una funzione di trovare al suo interno l'elemento che contiene la classe dropdown e di rimuovere la classe active
+    link.click(function(){
+        $(this).find('.dropdown').removeClass('active')
+    })
 })
